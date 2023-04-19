2f592133e9fc635d86598a74cec999df74ba7f53
artem <artemkbnv@yandex.ru>
2023-04-19 12:22:18 +0300

    проверка

--- src/test/java/com/example/testwork/TestWorkApplicationTests.java
+++ src/test/java/com/example/testwork/TestWorkApplicationTests.java
@@ -1,13 +1,74 @@
 package com.example.testwork;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(classes = User.class)
 class TestWorkApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+        @Autowired
+        private User user;
+
+        @Test
+        public void testUser() {
+                String expectedLogin = "name";
+                String expectedEmail = "arasa@yandex.ru";
+
+                assertEquals(expectedLogin, user.getLogin());
+                assertEquals(expectedEmail, user.getEmail());
+        }
+        @Test
+        public  void  testCorrectEmail(){
+
+                User user = new User();
+
+                assertNull(user.getEmail());
+
+                String expectedEmail ="arasa@yandex.ru";
+                user.setEmail(expectedEmail);
+                assertEquals(expectedEmail, user.getEmail());
+
+                String invalidEmail = "arasayandex.ru";
+                user.setEmail(invalidEmail);
+
+                assertEquals("", user.getEmail());
+
+                String invalidEmail2 = "arasa@yandexru";
+                user.setEmail(invalidEmail2);
+
+
+                assertEquals("", user.getEmail());
+
+
+                String emptyEmail = "";
+                user.setEmail(emptyEmail);
+
+
+                assertEquals("", user.getEmail());
+
+
+        }
+        @Test
+        public  void  loginNotEqialEmail(){
+                User user = new User();
+                user.setLogin("name");
+                user.setEmail("arasa@yandexru");
+                assertNotEquals(user.getLogin(), user.getEmail());
+        }
+
+        @Test
+        public void userCreatNull(){
+                User user = new User();
+                assertNotNull(user);
+                assertNull(user.getLogin());
+                assertNull(user.getEmail());
+                }
+
+
 
 }
